827422b1350efe209c4581f62f8c815167b1c724
Alberto Martínez <alberto@a81.biz>
2024-09-22 13:31:13 -0600

    app name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Landing Page - PAM';
+  }, []); 
+
   return (
     <div className="App" style={{ height: '100vh', overflow: 'hidden' }}>
       <iframe 
