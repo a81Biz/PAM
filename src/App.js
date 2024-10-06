30e9f68b21bd4b139be2966e983c1f3c426999e4
Alberto Martínez <alberto@a81.biz>
2024-10-05 18:17:52 -0600

    Infographic-demo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Usa HashRouter
 import Home from './components/Home';
 import Playground from './components/Playground';
+import Infographic from './components/infographic';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} /> 
         <Route path="/playground" element={<Playground />} /> 
+        <Route path="/infographic" element={<Infographic />} /> 
       </Routes>
     </Router>
   );
---
+++ src/components/infographic.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+
+function Infographic() {
+  useEffect(() => {
+    document.title = 'AM | Infographic'; 
+  }, []);
+
+  return (
+    <div style={{ height: '100vh', overflow: 'hidden' }}>
+      <iframe
+        src="./infographic-demo/public/index.html" 
+        style={{ width: '100%', height: '100%', border: 'none' }}
+        title="Infographic"
+      ></iframe>
+    </div>
+  );
+}
+
+export default Infographic;
