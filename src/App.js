353c15f8a6408dc8f945234432c43ca9e8a52e26
Alberto Martínez <alberto@a81.biz>
2024-10-06 01:37:04 -0600

    components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Usa HashRouter
 import Home from './components/Home';
 import Playground from './components/Playground';
-import Infographic from './components/infographic';
 
 function App() {
   return (
