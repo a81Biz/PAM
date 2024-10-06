a3abe44703668cf6fa106de21546075f11f945fc
Alberto Martínez <alberto@a81.biz>
2024-10-06 00:49:14 -0600

    path infographic

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} /> 
         <Route path="/playground" element={<Playground />} /> 
-        <Route path="/infographic" element={<Infographic />} /> 
       </Routes>
     </Router>
   );
